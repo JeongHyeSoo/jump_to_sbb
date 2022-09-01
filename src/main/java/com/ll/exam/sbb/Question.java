534a5de40cd20cb72f3366142ebda49b67f8450e
JeongHyeSoo <nailonly@naver.com>
2022-09-01 15:10:57 +0900

    빠진 파일 추가 +질문 가져올 때, 답변들도 같이 가져오도록

--- src/main/java/com/ll/exam/sbb/Question.java
+++ src/main/java/com/ll/exam/sbb/Question.java
@@ -22,6 +22,6 @@ public class Question {
     private String content;
     private LocalDateTime createDate;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
     private List<Answer> answerList;
 }
