7f994929539616a5626f41779b52b98750688cf8
JeongHyeSoo <nailonly@naver.com>
2022-09-02 13:29:32 +0900

    / 로 접속시 /question/list 로 302 리다이렉트 하도록

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -208,6 +208,11 @@ public class MainController {
     Person addPerson(Person p){
         return p;
     }
+
+    @RequestMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
 }
 
 @AllArgsConstructor
